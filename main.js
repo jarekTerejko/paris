211f33b3c3f01836580690662a0632951abbee7d
Jarosław Terejko <j.terejko@gmail.com>
2019-03-25 18:32:19 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+//$('.navbar a').on('click', function (e) {
+//
+//	if (this.hash !== '') {
+//		const hash = this.hash;
+//		e.preventDefault();
+//		$('.scroll-sections').animate({
+//			scrollTop: $(hash).offset().top
+//		}, 600);
+//	}
+//});
